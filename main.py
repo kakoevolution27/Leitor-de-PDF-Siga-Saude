8d690dab305359526b7c6d2fd5d315944b8c038e
kaio <indie3212@gmail.com>
2025-01-17 13:10:54 -0300

    git comiit -m ":sparkles: feat: caixa de dialogo para selecionar a pasta onde estão os arquivos
    *IMPLEMENTAÇÂO FUNCIONAL*
    *MINIMO TESTADO*
    
    :construction: IMPLEMENTADO EM URGENCIA PARA USO CONSTRUIR MODULO E TESTAR FUNÇÃO"

--- main.py
+++ main.py
@@ -1,18 +1,36 @@
 from classi import ManipuladorDePdf, LeituraDeArquivoPdfAgenda, FormatadorDeDados, EscritorDeTexto
 import pandas as pd
+import os 
+import tkinter as tk
+from tkinter import filedialog
 
-caminho_do_txt = fr""
+def recuperar_caminho_do_arquivo(path_pasta):
+    paths = []
+    for root, dirs, files in os.walk(path_pasta):
+        for file in files:
+            if file.endswith('.pdf'):
+                caminho_completo = os.path.join(root,file)
+                paths.append(caminho_completo)
+    return paths
 
-path = []
-caminhos = open(caminho_do_txt).readlines()
+def escolher_pasta():
+    root = tk.Tk()
+    root.withdraw()
 
-for texto in caminhos:
-    path.append(fr"{texto.replace("\n", "")}")
+    pasta_selecionada = filedialog.askdirectory(title="selecione a pasta de destino")
+
+    if pasta_selecionada:
+        return os.path.join(pasta_selecionada)
+    else:
+        return None    
+
+pasta_selecionada = escolher_pasta()
+path = recuperar_caminho_do_arquivo(pasta_selecionada)
 
 
-def main():
+def main() -> None:
     for i , p in enumerate(path):
-        caminho_destino = fr""
+        caminho_destino = fr"C:\Users\Kaio\Desktop\RESIDUOS\dados {i}.csv"
         manipuladorDeArquivos = ManipuladorDePdf()
         leitorDeArquivos = LeituraDeArquivoPdfAgenda()
         formatadorDeDados = FormatadorDeDados()
@@ -48,6 +66,7 @@ def main():
         #escritor.escrever_texto()
         df.to_csv(caminho_destino, index=False, sep=";")
         manipuladorDeArquivos.fechar_arquivo(pdf)
+    return None
     
 
 if __name__ == "__main__":
